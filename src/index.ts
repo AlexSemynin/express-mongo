1804d92dd77706bd71befd9f6bb0ed3516459cd6
AlexSemynin <Semynin@TopSystems.ru>
2022-07-23 20:35:11 +0300

    add fileupload middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import env from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
 
 import {postRouter} from './endpoints/postRouter';
 
@@ -19,6 +20,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api", postRouter);
+app.use(fileUpload());
 
 const DB_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
