255153a34413bbf076d74da36ead43fdb6cd0d6e
AlexSemynin <Semynin@TopSystems.ru>
2022-07-23 17:20:17 +0300

    подключение к mongodb

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,41 @@ const PORT = process.env.PORT || 5000;
 console.log(process.pid);
 console.log(process.env.NODE_ENV);
 
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+} = process.env;
+
+const jsonParser = express.json();
+
 const app = express();
-const mongoClient = new MongoClient('mongodb://localhost:27017');
+const mongoClient = new MongoClient(`mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/`);
 
 (async () => {
   try {
-     await mongoClient.connect();
-     app.locals.collection = mongoClient.db("usersdb").collection("users");
-     await app.listen(PORT, () => console.log(`server started on ${PORT}`));
-     console.log("Сервер ожидает подключения...");
+    await mongoClient.connect();
+    app.locals.collection = mongoClient.db("usersdb").collection("users");
+    await app.listen(PORT, () => console.log(`server started on ${PORT}`));
+    console.log("Сервер ожидает подключения...");
  }catch(err) {
-     return console.log(err);
+    return console.log(err);
  } 
 })();
+
+app.post("/api/users", jsonParser, function (req, res) {
+       
+    if(!req.body) return res.sendStatus(400);
+       
+    const userName = req.body.name;
+    const userAge = req.body.email;
+    const user = {name: userName, email: userAge};
+       
+    const collection = req.app.locals.collection;
+    collection.insertOne(user, function(err: any, result: any){
+               
+        if(err) return console.log(err);
+        res.send(user);
+    });
+});
