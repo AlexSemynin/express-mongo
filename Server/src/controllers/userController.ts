c35b6e0c8cc2fb6fb5920fdc740e47241d3cdb66
AlexSemynin <Semynin@TopSystems.ru>
2022-07-24 20:37:58 +0300

    add user controller

---
+++ Server/src/controllers/userController.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import User, { IUserDto, IUser } from '../dto/user';
+import {StatusError, userService} from '../services/userService';
+import bcript from 'bcrypt';
+import {check, validationResult} from 'express-validator';
+
+class UserConteroller {
+  public async signIn (req: express.Request, resp: express.Response) {
+    try{
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+        return resp.status(400).json({ errors: errors.array() }); 
+      }
+      const { email, password }: IUser = req.body;
+      const user = await userService.tryCreateUser(email, password);
+      resp.status(200).json(user);
+    } catch(e: any) {
+      resp.status(500).json(e.message);
+    }
+  }
+
+  public async logIn(req: express.Request, resp: express.Response) {
+    try{
+      const { email, password }: IUser = req.body;
+      const {token, user} = await userService.tryGetUser(email, password);
+      return resp.status(200).json({token, user});
+    } catch (e: any) {
+      if(e instanceof StatusError) {
+        return resp.status(e.StatuseCode).json(e.message);
+      }
+      return resp.status(500).json(e.message);
+    }
+  }
+}
+
+
+export const userController = new UserConteroller();
